ce27f1464c6f757bd86d20950e22d9bea82dfdb3
KathrynH13 <kathryn.harris@ucas-edu.net>
2024-09-10 19:28:02 +0000

    saving practice

--- Test.c
+++ Test.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 int main(void){
-    char bestBook[] = "Harry Potter";
-    printf("%s" , bestBook);
+    char book[30];
+    printf("Tell me your favorite book: \n");
+    fgets(book, sizeof(book), stdin);
+    printf("I love %s" , book);
+     
     return 0;
 }
