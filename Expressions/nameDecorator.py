1f745cf1cbd04873c93e9703b061583d45dfe76d
KathrynH13 <kathryn.harris@ucas-edu.net>
2024-09-17 19:45:09 +0000

    Name Decorater-python

---
+++ Expressions/nameDecorator.py
@@ -0,0 +1,12 @@
+name= input("Welcome to pretty name generator! Please paste your name: ")
+
+print("<<<" , name , ">>>")
+print("(((" , name , ")))")
+print("---" , name , "---")
+print(":) :)" , name , ":) :)")
+print("###" , name , "###")
+print("+++" , name , "+++")
+print("~~~ " , name , "~~~ ")
+print("===" , name , "===")
+
+ 
