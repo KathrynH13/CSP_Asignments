b8509383b5b8a22815c7e435372023a0e0c255f0
KathrynH13 <kathryn.harris@ucas-edu.net>
2024-09-19 19:45:07 +0000

    pusshh

---
+++ Strings/sillySentances.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int main(void){
+char animal[20];
+char place[20];
+char verb[20];
+char sentance[400] = "The %s";
+printf("name an animal: ");
+scanf("%s",animal);
+printf("Name a place: ");
+scanf("%s" , place);
+printf("Name a past tense verb: ");
+scanf(verb, sizeof(verb), stdin);
+strcat(sentance, animal);
+strcat(sentance, "went to the");
+strcat(sentance, place);
+strcat(sentance,"and");
+strcat(sentance, verb);
+strcat(sentance, "with his friend the angry turtle.");
+printf("%s", sentance);
+return 0;
+}
