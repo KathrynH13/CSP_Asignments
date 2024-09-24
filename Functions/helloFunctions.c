8782706bbbc9410d27a44688822dee04e5872fe3
KathrynH13 <kathryn.harris@ucas-edu.net>
2024-09-24 19:36:43 +0000

    functions c

---
+++ Functions/helloFunctions.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void){
+    printf("\n Hello Kathryn");
+    printf("\n Hello Kent");
+    printf("\n Hello Caleb");
+    printf("\n Hello Richard");
+    printf("\n Hello Kyla");
+  
+return 0;
+}
