6044b21787cfde4c58a30cd587b550ec6c191836
KathrynH13 <kathryn.harris@ucas-edu.net>
2024-09-24 19:33:03 +0000

    finiched hello world

---
+++ Functions/helloFuctions.py
@@ -0,0 +1,7 @@
+def hello(name):
+    return f"Hello{name}!"
+print(hello (" Kathryn"))
+print(hello (" Kent"))
+print(hello (" Richard"))
+print(hello (" Kyla"))
+print(hello (" Caleb"))
---
+++ Test Folder/random.py
@@ -0,0 +1,2 @@
+book=input("Yo wsp please say yes or no: \n ")
+love=input("GOOD CHOICE BETTER LUCK NEXT TIME HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHHAHAHAHAHAHAHAHAHA PLease do u wish to continue?: \n ")
