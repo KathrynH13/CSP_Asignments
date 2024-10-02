dae5bcd338332bcc77bd04aae4fbebaa5dddc7c3
KathrynH13 <kathryn.harris@ucas-edu.net>
2024-10-02 19:36:21 +0000

    real time

--- Expressions/Conditionals/time_of_day.c
+++ Expressions/Conditionals/time_of_day.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <time.h>
+int hour;
 int main ()
 {
 time_t rawtime; 
@@ -7,5 +8,14 @@ struct tm * timeinfo;
 time (&rawtime);
 timeinfo = localtime(&rawtime);
 printf("Current local time and date: %s", asctime(timeinfo));
+printf("tell me the hour in military time!: \n");
+scanf("%d", &hour);
+if(hour < 12 ){
+        printf("Good Morning");
+}else if (hour < 18){
+        printf("Good Afternoon ");
+}else{
+        printf("Good Evening ");
+}
 return 0;
 }
