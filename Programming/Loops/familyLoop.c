065b813d317a50a8b3ec5fd5128eb8d880718675
KathrynH13 <kathryn.harris@ucas-edu.net>
2024-10-09 19:31:45 +0000

    update c

--- Programming/Loops/familyLoop.c
+++ Programming/Loops/familyLoop.c
@@ -4,9 +4,9 @@ int i;
 int main(){
    for(i=0;i<8;i++){
        if(strcmp(sibs[i],"Kyla")==0){
-           printf("%s Spackman\n", sibs[i]);
+           printf("Hello %s Spackman\n", sibs[i]);
        }else{
-       printf("%s Harris \n" , sibs[i]);
+       printf("Hello %s Harris \n" , sibs[i]);
        }
 };
 
--- Programming/Loops/my_family_loop.py
+++ Programming/Loops/my_family_loop.py
@@ -1,1 +1,16 @@
+import random
+count= 0
+siblings = ["Louisnea", "Richard", "Kent", "Kyla", "Caleb"]
+
+
+while count < len(siblings):
+   print(siblings[count])
+   count+=1
+
+ 
+ 
+
+
+
+
 
