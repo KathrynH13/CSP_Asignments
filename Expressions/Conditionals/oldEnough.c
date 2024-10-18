b61f1844906b1601591d65ce47e1273cdeede84d
KathrynH13 <kathryn.harris@ucas-edu.net>
2024-10-18 02:04:31 +0000

    FIZZBUZ

---
+++ Expressions/Conditionals/oldEnough.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+int age = 18;
+bool vote  = true;
+int main(void){
+        if (age >= 18 && vote != false){
+        printf("You are old enough to vote\n");
+}else if (age >= 16){
+        printf("You are old enough to drive\n");
+}else if (age >= 5) {
+        if (age ==15){
+        printf("You are old enough to get a learners permit");
+        }
+}else if (age >= 5)
+        if(age == 15){
+        printf("You are old enough to go to school \n)");
+}else{
+       printf("Sorry you are to young to go to school\n");
+
+return 0;
+}
+
